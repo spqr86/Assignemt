47e0d25581acc5aaf3ddac8f407ad5edfa15ad67
test <test@IRITOASULIL20C>
2019-02-21 11:23:41 +0000

    Correct datagridView

--- WindowsApp1/Form1.vb
+++ WindowsApp1/Form1.vb
@@ -6,45 +6,43 @@ Public Class lblMathsforIT
     Dim connection As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source= ..\..\resources\SkillsDemo#2.accdb;Persist Security Info=True")
     Dim id As Single
     Dim CAO As Double
-    Dim operatingResult As Short
-    Dim networkingResult As Short
-    Dim computerResult As Short
-    Dim virtualisationResult As Short
-    Dim programmingResult As Short
-    Dim mathResult As Short
-    Dim databaseResult As Short
-    Dim communicationResult As Short
-    Dim workExperienceResult As Short
+    Dim operatingResult As Double
+    Dim networkingResult As Double
+    Dim computerResult As Double
+    Dim virtualisationResult As Double
+    Dim programmingResult As Double
+    Dim mathResult As Double
+    Dim databaseResult As Double
+    Dim communicationResult As Double
+    Dim workExperienceResult As Double
     Dim totalResult As Double
     Dim rowNumber As Short
 
     Private Sub btnShow_Click(sender As Object, e As EventArgs) Handles btnShow.Click
         dataSet.Clear()
-
-        adapter.SelectCommand = New OleDbCommand("Select PPSN, FirstName + ' ' + Surname As [Name], OpeartingSystems, NetworkingEssentials, ComputerSystemsHardware, VirtualisationSupport, 
-                                                ProgrammingandDesignPrinciples, MathsforIT, DatabaseMethods, Communications, WorkExperience
-                                                from [5M0536 Module Results]", connection)
+        If DataGridView1.Columns.Contains("Total Cao") Then
+            DataGridView1.Columns.RemoveAt(12)
+        End If
+        adapter.SelectCommand = New OleDbCommand("Select * from [5M0536 Module Results]", connection)
         adapter.Fill(dataSet, "[5M0536 Module Results]")
         DataGridView1.DataSource = dataSet.Tables("[5M0536 Module Results]")
+
         DataGridView1.Columns.Add("Total Cao", "Total Cao")
-
-
         rowNumber = DataGridView1.RowCount
-        MsgBox(rowNumber)
 
         For i = 0 To rowNumber - 2
-            operatingResult = dataSet.Tables("[5M0536 Module Results]").Rows(i).Item(2)
-            networkingResult = dataSet.Tables("[5M0536 Module Results]").Rows(i).Item(3)
-            computerResult = dataSet.Tables("[5M0536 Module Results]").Rows(i).Item(4)
-            virtualisationResult = dataSet.Tables("[5M0536 Module Results]").Rows(i).Item(5)
-            programmingResult = dataSet.Tables("[5M0536 Module Results]").Rows(i).Item(6)
-            mathResult = dataSet.Tables("[5M0536 Module Results]").Rows(i).Item(7)
-            databaseResult = dataSet.Tables("[5M0536 Module Results]").Rows(i).Item(8)
-            communicationResult = dataSet.Tables("[5M0536 Module Results]").Rows(i).Item(9)
-            workExperienceResult = dataSet.Tables("[5M0536 Module Results]").Rows(i).Item(10)
+            operatingResult = dataSet.Tables("[5M0536 Module Results]").Rows(i).Item(3)
+            networkingResult = dataSet.Tables("[5M0536 Module Results]").Rows(i).Item(4)
+            computerResult = dataSet.Tables("[5M0536 Module Results]").Rows(i).Item(5)
+            virtualisationResult = dataSet.Tables("[5M0536 Module Results]").Rows(i).Item(6)
+            programmingResult = dataSet.Tables("[5M0536 Module Results]").Rows(i).Item(7)
+            mathResult = dataSet.Tables("[5M0536 Module Results]").Rows(i).Item(8)
+            databaseResult = dataSet.Tables("[5M0536 Module Results]").Rows(i).Item(9)
+            communicationResult = dataSet.Tables("[5M0536 Module Results]").Rows(i).Item(10)
+            workExperienceResult = dataSet.Tables("[5M0536 Module Results]").Rows(i).Item(11)
             totalResult = calculateCAO(operatingResult) + calculateCAO(networkingResult) + calculateCAO(computerResult) + calculateCAO(virtualisationResult) + calculateCAO(programmingResult) +
             calculateCAO(mathResult) + calculateCAO(databaseResult) + calculateCAO(communicationResult) + calculateCAO(workExperienceResult)
-            DataGridView1.Rows(i).Cells(11).Value = totalResult
+            DataGridView1.Rows(i).Cells(12).Value = totalResult
         Next
 
     End Sub
@@ -52,12 +50,15 @@ Public Class lblMathsforIT
     Private Sub btnOneStudent_Click(sender As Object, e As EventArgs) Handles btnOneStudent.Click
         Dim text As String = txtOneStudent.Text
         dataSet.Clear()
+        If DataGridView1.Columns.Contains("Total Cao") Then
+            DataGridView1.Columns.RemoveAt(12)
+        End If
         adapter.SelectCommand = New OleDbCommand("Select * from [5M0536 Module Results] where PPSN = '" & text & "'", connection)
         adapter.Fill(dataSet, "Search Result")
         DataGridView1.DataSource = dataSet.Tables("Search Result")
         DataGridView1.Columns.Add("Total Cao", "Total Cao")
 
-        operatingResult = dataSet.Tables("Search Result").Rows(0).Item(3)
+            operatingResult = dataSet.Tables("Search Result").Rows(0).Item(3)
         networkingResult = dataSet.Tables("Search Result").Rows(0).Item(4)
         computerResult = dataSet.Tables("Search Result").Rows(0).Item(5)
         virtualisationResult = dataSet.Tables("Search Result").Rows(0).Item(6)
@@ -73,13 +74,16 @@ Public Class lblMathsforIT
 
     Private Sub btnSearchLike_Click(sender As Object, e As EventArgs) Handles btnSearchLike.Click
         dataSet.Clear()
+        If DataGridView1.Columns.Contains("Total Cao") Then
+            DataGridView1.Columns.RemoveAt(12)
+        End If
         Dim text As String = txtSerchNSP.Text
         Dim quert As String = "Select * from [5M0536 Module Results] where FirstName like '%" & text & "%' or Surname like '%" & text & "%' or PPSN like '%" & text & "%'"
         adapter.SelectCommand = New OleDbCommand(quert, connection)
         adapter.Fill(dataSet, "Search Result")
         DataGridView1.DataSource = dataSet.Tables("Search Result")
         DataGridView1.Columns.Add("Total Cao", "Total Cao")
-        rowNumber = DataGridView1.RowCount
+            rowNumber = DataGridView1.RowCount
 
         For i = 0 To rowNumber - 2
             operatingResult = dataSet.Tables("Search Result").Rows(i).Item(3)
@@ -125,4 +129,4 @@ Public Class lblMathsforIT
 
 End Class
 
-'hello world
+
